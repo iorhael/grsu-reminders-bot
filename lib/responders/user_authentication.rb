3099adeff8d8fbec9073f5e6330259fe1df98912
Egor Khanko <egorkhanko@gmail.com>
2023-05-28 22:50:59 +0300

    [DEV] Final changes to user login flow

--- lib/responders/user_authentication.rb
+++ lib/responders/user_authentication.rb
@@ -13,33 +13,27 @@ class Responders
     end
 
     def choose_department
-      kb = []
-      ::Department.all.each do |department|
-        { department: department.id }.to_json
-        kb.push([Telegram::Bot::Types::InlineKeyboardButton.new(text: department.title, callback_data: { department: department.id }.to_json)])
+      kb = ::Department.required.all.map do |department|
+        [Telegram::Bot::Types::InlineKeyboardButton.new(text: department.title, callback_data: { department: department.id }.to_json)]
       end
       markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
       bot.api.send_message(chat_id: chat_id, text: 'Choose department', reply_markup: markup)
     end
 
     def choose_faculty
-      kb = []
-      ::Faculty.all.each do |faculty|
-        { faculty: faculty.id }.to_json
-        kb.push([Telegram::Bot::Types::InlineKeyboardButton.new(text: faculty.title, callback_data: { faculty: faculty.id }.to_json)])
-      end
+      kb = ::Faculty.all.map do |faculty|
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: faculty.title, callback_data: { faculty: faculty.id }.to_json)
+      end.each_slice(2).to_a
       markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
       bot.api.send_message(chat_id: chat_id, text: 'Choose faculty', reply_markup: markup)
     end
 
     def choose_group
-      kb = []
-      ::Group.where(department_id: user.department_id, faculty_id: user.faculty_id).each do |group|
-        { group: group.id }.to_json
-        kb.push([Telegram::Bot::Types::InlineKeyboardButton.new(text: group.title, callback_data: { group: group.id }.to_json)])
-      end
+      kb = ::Group.sorted.where(department_id: user.department_id, faculty_id: user.faculty_id).map do |group|
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: group.title, callback_data: { group: group.id }.to_json)
+      end.each_slice(2).to_a
       markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-      bot.api.send_message(chat_id: chat_id, text: 'Choose group', reply_markup: markup)
+        bot.api.send_message(chat_id: chat_id, text: 'Choose group', reply_markup: markup)
     end
 
     def welcome_message
--- models/department.rb
+++ models/department.rb
@@ -3,4 +3,5 @@
 require 'active_record'
 
 class Department < ActiveRecord::Base
+  scope :required, -> { where.not(title: 'соискательство') }
 end
