2a8ae93a288a03446142851795f1306615a7be03
Egor Khanko <egorkhanko@gmail.com>
2023-05-28 22:30:41 +0300

    [DEV] Add user notifications by schedule

--- models/group.rb
+++ models/group.rb
@@ -6,5 +6,7 @@ class Group < ActiveRecord::Base
   belongs_to :faculty
   belongs_to :department
 
+  has_many :students, class_name: 'User'
+
   scope :sorted, -> { order(title: :asc) }
 end
--- services/data/updater/lessons.rb
+++ services/data/updater/lessons.rb
@@ -3,13 +3,16 @@
 class Data
   class Updater
     class Lessons < Data::Updater::Base
-      attr_reader :date, :group_id
+      attr_reader :date, :group_id, :bot, :groups_lessons_with_exams
 
-      def initialize(json_record, group_id)
+      def initialize(json_record, group_id, bot:)
         @json_record = json_record
 
         @group_id = group_id
         @date = Date.parse(json_record[:date])
+        @bot = bot
+
+        @groups_lessons_with_exams = []
       end
 
       def call
@@ -25,14 +28,43 @@ class Data
 
           lesson.assign_attributes(attributes_to_update)
           lesson.teacher_id = teacher_id
+
+          groups_lessons_with_exams << lesson_json if lesson[:type].match?(/защита|в устной форме|в письменной форме/)
+
           lesson.save
         end
+
+        notify_group_about_exams
       end
 
       private
 
       def at_lesson_date(time)
         DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec, time.zone)
+      end
+
+      def notify_group_about_exams
+        notification_texts = groups_lessons_with_exams.map { |lesson| generate_lesson_notification_text(lesson) }
+        notification_text = notification_texts.join("\n\n")
+
+        notify_group_with(group_id, notification_text)
+      end
+
+      def generate_lesson_notification_text(lesson)
+        [
+          "(#{lesson[:timeStart]}-#{lesson[:timeEnd]}) - *#{lesson[:title]}*",
+          "_#{lesson[:teacher][:fullname]} - #{lesson[:type]}, #{lesson[:address]}, #{lesson[:room]}_"
+        ].join("\n")
+      end
+
+      def notify_group_with(group_id, notification_text)
+        ::Group.find(group_id).students.where(notificate: true).each do |student|
+          notify(student, notification_text)
+        end
+      end
+
+      def notify(user, text)
+        bot.api.send_message(chat_id: user.uid, text:, parse_mode: :Markdown)
       end
     end
   end
